bc0a41c324a57adf5d98afd4a22ee553e823a777
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-20 11:51:08 -0600

    Added MovieData.ms Model to match JSON Document format

---
+++ BlockbusterRentals/BlockbusterRentals/Models/MovieData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BlockbusterRentals.Models
+{
+    public class MovieData
+    {
+
+        public string Title { get; set; }
+
+
+        [Display(Name = "Release Date")]
+        public string Released { get; set; }
+        
+        public string Runtime { get; set; }
+
+        public string Genre { get; set; }
+
+
+        [Display(Name = "Cast")]
+
+        public string Actors { get; set; }
+
+        [Display(Name = "Description")]
+
+        public string Plot { get; set; }
+
+        public string imdbId { get; set; }
+
+
+        [Display(Name = "Rating / 5")]
+
+        public string imdbRating { get; set; }
+
+
+
+
+
+    }
+}
