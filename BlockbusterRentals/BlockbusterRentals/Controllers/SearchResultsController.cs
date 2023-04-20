bf43c18e4580da340c101c48913ab2df8e8faea3
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-20 13:01:18 -0600

    First Search attempt: but needs JSON doc reformatting first
    Query method is in the Controller to pass the results model to the view to dsiplay in a table. But, the JSON first needs to be corrected to be properly indexed

--- BlockbusterRentals/BlockbusterRentals/Controllers/SearchResultsController.cs
+++ BlockbusterRentals/BlockbusterRentals/Controllers/SearchResultsController.cs
@@ -1,17 +1,56 @@
-﻿using System;
+﻿
+
+
+
+
+using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Nest;
+using BlockbusterRentals.Models;
 
 namespace BlockbusterRentals.Controllers
 {
     public class SearchResultsController : Controller
     {
+
+        // Set up ElasticSearch
+
+        ElasticClient client = new ElasticClient(new ConnectionSettings(new Uri("http://localhost:9200"))
+            .DefaultIndex("Index/movieData"));
+
+
+
         // GET: SearchResults
         public ActionResult Index()
         {
-            return View();
+            Console.WriteLine("Starting query");
+            // Structure teh search query
+            var searchResponse = client.Search<SearchResult>(s => s
+            .Query(q => q
+                .Match(m => m
+                    .Field(f => f.Title)
+                    .Query("Her Wicked Ways")
+                    )
+                )
+            );
+
+            // Extract the search results into the MVC domain model SearchResult
+            List<SearchResult> results = new List<SearchResult>();
+            Console.WriteLine("Showing hits");
+
+            foreach (var hit in searchResponse.Hits)
+            {
+                Console.WriteLine("Hit:");
+
+                results.Add(hit.Source);
+            }
+
+            return View(results);
         }
     }
 }
+
