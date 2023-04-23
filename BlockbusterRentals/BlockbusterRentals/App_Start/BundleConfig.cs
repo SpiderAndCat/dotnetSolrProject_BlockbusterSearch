5e21fa3c5c68e9aa1c5c4d105bb48038b9bd2b01
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-22 20:15:48 -0600

    Bootstrap-quartz theme applied

--- BlockbusterRentals/BlockbusterRentals/App_Start/BundleConfig.cs
+++ BlockbusterRentals/BlockbusterRentals/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace BlockbusterRentals
     
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-quartz.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css"));
         }
